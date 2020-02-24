cffc6bb0c5cd3311f22cf442485f6257fca77701
Magnus Birkemose <mnorda@student.aau.dk>
2020-02-24 08:19:24 +0100

    Preprocessing script

--- Scripts/Preprocessing.R
+++ Scripts/Preprocessing.R
@@ -4,9 +4,13 @@ source("./Scripts/Functions.R")
 
 ### Preprocessing ------------------------------------------------------------
 # path <- "/Volumes/Samsung_T5/highFrequencyData/SPY"
-path <- "/Volumes/Samsung_T5/SPY2000-2001"
+path <- "~/Desktop/P8/SPY2000-2001"
 
 dataPreprocessed <- funPreprocessing(path)
 
 ### Write csv ----------------------------------------------------------------
-write.csv(dataPreprocessed, )
+write.csv(
+  dataPreprocessed, 
+  paste(path, "SpyPreprocessed.csv", "/"), 
+  row.names = FALSE
+)

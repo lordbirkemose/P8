0fdce9db595d49ab0bf0484cee7d901d70b4c512
unknown <jstran16@student.aau.dk>
2020-03-12 16:31:29 +0100

    Changed lag to lead for RV.1.Ahead. Led to garbage results, but should be correct

--- Scripts/HARModels.R
+++ Scripts/HARModels.R
@@ -2,11 +2,6 @@ library(tidyverse)
 library(magrittr)
 data <- as_tibble(read.csv("./Data/SpyCleaned.gz"))
 
-aggr_return <- function(price, sgn){
-  
-}
-
-
 data %<>% mutate(Log.Price = log(Price),
                  Start = as.POSIXct(Start, format = "%F") ) %>%
   group_by(Start) %>%
@@ -16,33 +11,19 @@ data %<>% mutate(Log.Price = log(Price),
             Negative.Return = min(c(Return, 0)) ) %>%
   mutate(RV.Weekly  = zoo::rollmean(RV.Daily, k = 5,  align = "right", fill = NA),
          RV.Monthly = zoo::rollmean(RV.Daily, k = 22, align = "right", fill = NA),
-         RV.1.Ahead = dplyr::lag(RV.Daily)) %>%
-  # select(-Return) %>%
+         RV.1.Ahead = dplyr::lead(RV.Daily), 
+         Direction  = as.numeric( RV.Daily/dplyr::lag(RV.Daily) > 1) ) %>%
+  select(Start, RV.1.Ahead, RV.Daily, RV.Weekly, RV.Monthly, 
+         Positive.Return, Negative.Return, Direction) %>%
   drop_na(); data
 
 Base.HAR <- data %$% lm(RV.1.Ahead ~ RV.Daily + 
                                      RV.Weekly + 
                                      RV.Monthly)
-summary(Base.HAR)
 
-# The residual vs fitted plot has a megaphone shape indicating
-# we should regress |eps| ~ RV.1.Ahead to obtain w_{ii}'s as inverses
-# of fitted values
-# w <- data %$% lm( abs( residuals(Base.HAR) ) ~ RV.1.Ahead) %>%
-#   {1/predict(.)}
-# 
-# WLS.HAR <- data %$% lm(RV.1.Ahead ~ RV.Daily + 
-#                                     RV.Weekly + 
-#                                     RV.Monthly, weights = w)
-#
-# w <- 1/abs(predict(Base.HAR)) # Vi predicter en negativ observation 
-# WLS.RV.HAR <- data %$% lm(RV.1.Ahead ~ RV.Daily + 
-#                                     RV.Weekly + 
-#                                     RV.Monthly, weights = w)
-# 
-AR.HAR <- data %$% lm(RV.1.Ahead ~ RV.Daily +
-                                   RV.Weekly +
-                                   RV.Monthly +
-                                   Positive.Return +
-                                   Negative.Return)
-
+WLS.HAR.L.D <- data %$% lm(RV.1.Ahead ~ RV.Daily +
+                                        RV.Weekly +
+                                        RV.Monthly +
+                                        Positive.Return +
+                                        Negative.Return +
+                                        Direction , weights = 1/predict(Base.HAR))  

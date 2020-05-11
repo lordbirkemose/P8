6c5f868af98acae4f14dd537bed18c9b5456a590
Magnus Birkemose <mnorda@student.aau.dk>
2020-05-11 11:54:39 +0200

    Montly leverage made to daily

--- Scripts/Functions.R
+++ Scripts/Functions.R
@@ -495,66 +495,67 @@ funAcfPlot <- function(data, lag.max, include.lag.zero = TRUE) {
 
 ### Get data for HAR ---------------------------------------------------------
 funGetDataHAR <- function(log = FALSE, test = FALSE) {
-  predDirectionTestXGB <- read.csv("./Data/predDirectionTestXGB.csv") %>%
+  if (test) {
+    predDirectionTestXGB <- read.csv("./Data/predDirectionTestXGB.csv") %>%
+      tibble::as_tibble() %>%
+      dplyr::mutate(Start = as.POSIXct(Start, format = "%F")) 
+  }
+  
+  data <- read.csv("./Data/SpyCleaned.gz") %>%
     tibble::as_tibble() %>%
     dplyr::mutate(
       Start = as.POSIXct(Start, format = "%F"),
-    )
-    data <- read.csv("./Data/SpyCleaned.gz") %>%
-      tibble::as_tibble() %>%
-      dplyr::mutate(
-        Start = as.POSIXct(Start, format = "%F"),
-        Log.Price = log(Price),
-        Abs.Diff.Price = abs(c(diff(Log.Price), 0)),
-        Abs.Diff.Price.Lag = dplyr::lag(Abs.Diff.Price)
-      ) %>%
-      { 
-        if (test) 
-          dplyr::filter(., Start >  "2007-09-30")
-        else
-          dplyr::filter(., Start <= "2007-09-30")
-      } %>%
-      dplyr::group_by(Start) %>%
-      dplyr::summarise(
-        RV.Daily = sum(diff(Log.Price)^2),
-        Return = diff(Log.Price, lag = 390),
-        Pos.Return = max(c(Return, 0)),
-        Neg.Return = min(c(Return, 0)),
-        N = dplyr::n(),
-        BV.Daily = pi*N/(2*N-2)*sum(Abs.Diff.Price*Abs.Diff.Price.Lag)
-      ) %>%
-      dplyr::mutate(
-        RV.Weekly = zoo::rollmeanr(RV.Daily, k = 5, fill = NA),
-        RV.Monthly = zoo::rollmeanr(RV.Daily, k = 22, fill = NA),
-        RV.1.Ahead = dplyr::lead(RV.Daily),
-        RV.Direction = as.numeric(RV.Daily/dplyr::lag(RV.Daily) > 1),
-        Pos.Return.Weekly = zoo::rollmeanr(Pos.Return, k = 5, fill = NA),
-        Neg.Return.Weekly = zoo::rollmeanr(Neg.Return, k = 5, fill = NA),
-        Jump.Daily = pmax(RV.Daily - BV.Daily, 0)
-      ) %>%
-      dplyr::left_join(., predDirectionTestXGB, by = "Start") %>%
-      dplyr::mutate(
-        RV.Direction = ifelse(
-          is.na(RVDirectionPred), RV.Direction, RVDirectionPred
+      Log.Price = log(Price),
+      Abs.Diff.Price = abs(c(diff(Log.Price), 0)),
+      Abs.Diff.Price.Lag = dplyr::lag(Abs.Diff.Price)
+    ) %>%
+    {
+      if (!test) dplyr::filter(., Start <= "2007-09-30") else .
+    } %>%
+    dplyr::group_by(Start) %>%
+    dplyr::summarise(
+      RV.Daily = sum(diff(Log.Price)^2),
+      Return = diff(Log.Price, lag = 390),
+      Pos.Return = max(c(Return, 0)),
+      Neg.Return = min(c(Return, 0)),
+      N = dplyr::n(),
+      BV.Daily = pi*N/(2*N-2)*sum(Abs.Diff.Price*Abs.Diff.Price.Lag)
+    ) %>%
+    dplyr::mutate(
+      RV.Weekly = zoo::rollmeanr(RV.Daily, k = 5, fill = NA),
+      RV.Monthly = zoo::rollmeanr(RV.Daily, k = 22, fill = NA),
+      RV.1.Ahead = dplyr::lead(RV.Daily),
+      RV.Direction = as.numeric(RV.Daily/dplyr::lag(RV.Daily) > 1),
+      Jump.Daily = pmax(RV.Daily - BV.Daily, 0)
+    ) %>%
+    {
+      if (test) {
+        dplyr::left_join(., predDirectionTestXGB, by = "Start") %>%
+        dplyr::mutate(
+          RV.Direction = ifelse(
+            is.na(RVDirectionPred), RV.Direction, RVDirectionPred
+          )
         )
-      ) %>%
-      dplyr::select(
-        Start, RV.1.Ahead, RV.Daily, RV.Weekly, RV.Monthly, Jump.Daily,
-        Pos.Return.Weekly, Neg.Return.Weekly, RV.Direction
-      )
-    if(log) {
-      data %<>%
-      dplyr::mutate(
-        RV.1.Ahead = log(RV.1.Ahead),
-        RV.Daily = log(RV.Daily),
-        RV.Monthly = log(RV.Monthly),
-        Jump.Daily = log(1 + Jump.Daily),
-        Pos.Return.Weekly = log(1 + Pos.Return.Weekly),
-        Neg.Return.Weekly = log(1 + abs(Neg.Return.Weekly))
-      )
-    }
+      } else .
+    } %>%
+    dplyr::select(
+      Start, RV.1.Ahead, RV.Daily, RV.Weekly, RV.Monthly, Jump.Daily,
+      Pos.Return, Neg.Return, RV.Direction
+    )
+    
+  if(log) {
+    data %<>%
+    dplyr::mutate(
+      RV.1.Ahead = log(RV.1.Ahead),
+      RV.Daily = log(RV.Daily),
+      RV.Monthly = log(RV.Monthly),
+      Jump.Daily = log(1 + Jump.Daily),
+      Pos.Return = log(1 + Pos.Return),
+      Neg.Return = log(1 + abs(Neg.Return))
+    )
+  }
 
-      data %>% 
-        tidyr::drop_na() %>%
-        return()
+  data %>% 
+    tidyr::drop_na() %>%
+    return()
 }

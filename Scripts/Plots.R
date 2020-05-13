6d5daa3dad4cdb6c13d45f2744407f8ba6535713
Magnus Birkemose <mnorda@student.aau.dk>
2020-05-13 08:46:28 +0200

    Moving plot stuff to Plots

--- Scripts/Plots.R
+++ Scripts/Plots.R
@@ -1,4 +1,4 @@
-### Packages -----------------------------------------------------------------
+### Preliminary --------------------------------------------------------------
 suppressPackageStartupMessages({
   library(tidyverse)
   library(lubridate)
@@ -7,6 +7,127 @@ suppressPackageStartupMessages({
 })
 
 source("./Scripts/Functions.R", echo = FALSE)
+
+# Colors
+# colors <- scales::hue_pal()(4)
+colors <- c("#fc8d62", "#779ecc", "#66c2a5", "#007e89", "#aec6cf")
+# scales::show_col(colors) # Run to see colors
+
+# Themes
+theme <- list(
+  ggplot2::theme_minimal(),
+  ggplot2::theme(
+    panel.grid.major = ggplot2::element_line(), 
+    panel.grid.minor = ggplot2::element_line(),
+    panel.background = ggplot2::element_blank(), 
+    axis.line = ggplot2::element_line(colour = "black")
+  )
+)
+themeLegend <- list(
+  ggplot2::theme_minimal(),
+  ggplot2::theme(
+    panel.grid.major = ggplot2::element_line(), 
+    panel.grid.minor = ggplot2::element_line(),
+    panel.background = ggplot2::element_blank(), 
+    axis.line = ggplot2::element_line(colour = "black"),
+    legend.position = "top" , 
+    legend.justification = "left" , 
+    legend.direction = "horizontal", 
+    legend.background = ggplot2::element_blank()
+  )
+)
+
+# ACF plot
+funAcfPlot <- function(data, lag.max = 50, include.lag.zero = FALSE) {
+  acfData <- stats::acf(data, lag.max = lag.max, plot = FALSE)
+  
+  acfData <- tibble::tibble(
+    lag = acfData$lag,
+    acf = acfData$acf
+  )
+  
+  if(!include.lag.zero) acfData %<>% dplyr::slice(-1)
+  
+  confInt <- qnorm((1 + 0.95)/2)/sqrt(length(data))
+  
+  plot <- ggplot(data = acfData, aes(x = lag, y = acf)) +
+    geom_hline(yintercept = 0) +
+    geom_segment(ggplot2::aes(xend = lag, yend = 0)) +
+    geom_hline(yintercept = confInt, linetype = 2, color = colors[2]) +
+    geom_hline(yintercept = -confInt, linetype = 2, color = colors[2]) +
+    labs(x = "Lag", y = "ACF") +
+    list(
+      theme_minimal(),
+      theme(
+        panel.grid.major = element_line(), 
+        panel.grid.minor = element_line(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black")
+      )
+    )
+  
+  return(plot)
+}
+
+# Results plot
+funResultsPlots <- function(data, labelsData) {
+  
+  data %<>% dplyr::filter(Start >= as.Date("2005-01-01"))
+  
+  textData <- tibble::tibble(
+    label = c(rep("In-sample", 4), rep("Out-of-sample", 4)),
+    x = c(rep(as.Date("2006-04-01"), 4), rep(as.Date("2008-11-01"), 4))
+  )
+  
+  p <- ggplot(data = data) +
+    geom_line(aes(x = Start, y = Value, color = Var)) +
+    labs(
+      # title = "OLS",
+      x = "Time",
+      y = "Value"
+    ) +
+    scale_colour_manual(
+      name = "",
+      labels = c(
+        "Actual Realized Volatility", "Predicted Realized Volatility"
+      ),
+      values = colors[2:3]
+    ) +
+    scale_x_date(
+      date_labels = "%Y",
+      breaks = pretty(data$Start, n = 5)
+    ) +
+    scale_y_continuous(
+      expand = expand_scale(mult = c(.1, .1))
+    ) +
+    themeLegend +
+    theme(strip.text.x = element_text(size = 10)) +
+    facet_wrap(
+      Type~ .,
+      scales = "free",
+      labeller = labeller(Type = labelsData)
+    ) +
+    annotate(
+      geom = "segment",
+      y = -Inf, yend = -Inf,
+      x = as.Date("2005-01-01"), xend = as.Date("2009-12-31"),
+      color = "grey",
+      size = 11
+    ) +
+    geom_text(
+      data = textData,
+      mapping = aes(x = x, y = -Inf, label = label),
+      vjust = -.5,
+      size = 3
+    ) +
+    geom_vline(
+      xintercept = as.Date("2007-10-01"),
+      linetype = "longdash",
+      color = "black"
+    )
+  
+  return(p)
+}
 
 ### Data comparison (WARNING: Needs raw data) --------------------------------
 path <- "~/Desktop/P8/SPY2000-2001" # Path to raw data
